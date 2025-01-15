737fe60fd5206710aae3727647f23ded58e95669
SillyMonkey1925 <84182084+SillyMonkey1925@users.noreply.github.com>
2025-01-15 09:43:16 +0000

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'benchmark-test';
+  intro: string = 'create a menu to display the test1 and test2 components';
+}
---
+++ src/app/test1/test1.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-test1',
+  imports: [],
+  templateUrl: './test1.component.html',
+  styleUrl: './test1.component.css'
+})
+export class Test1Component {
+  intro: string = 'Add the PrimeNG library to your project.';
+}
